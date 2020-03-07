ddc698b596ddd78982391d21f2f13fced7c29c95
Fighter-HUN <50296138+Fighter20000@users.noreply.github.com>
2020-03-07 12:08:59 +0100

    c# Source-code

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("Szia!");
+            int titkos = r.Next(1, 100);
+            Console.WriteLine("Gondoltam egy számra, ha szorzom önmagával, akkor "+titkos*titkos+"-t kapok!");
+
+            int szamom = Convert.ToInt32(Console.ReadLine());
+
+            while (szamom != titkos)
+            {
+                Console.WriteLine("Próbáld újra: ");
+                if(szamom > titkos)
+                {
+                    Console.WriteLine("Túl nagy számot adtál meg :D");
+                }
+                if(szamom < titkos)
+                {
+                    Console.WriteLine("Ettől nagyobb számot adj meg :D");
+                }
+                szamom = Convert.ToInt32(Console.ReadLine());
+                if (szamom == titkos)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Sikerült");
+
+        
+
+            Console.ReadKey();
+        }
+    }
+}
